9655c715227e96e661f1db6cb72d80e9c08c57fb
PENGJUN <pengjunlfy@qq.com>
2020-03-23 17:44:52 +0800

    Initial Commit

---
+++ PJ20200323/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PJ20200323
+//
+//  Created by Peng Jun on 2020/3/23.
+//  Copyright © 2020 Peng Jun. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
